9aabe1443127ec21439e504e535d4c4c768a94e3
Avishek Das <davishek7@gmail.com>
2021-03-14 21:09:46 +0530

    Some fixes

--- base/views.py
+++ base/views.py
@@ -65,8 +65,12 @@ class TaskCreate(LoginRequiredMixin,CreateView):
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
 	model=Task
-	fields='__all__'
+	fields=['title','description','complete']
 	success_url=reverse_lazy('tasks')
+
+	def form_valid(self,form):
+		form.instance.user=self.request.user
+		return super(TaskCreate,self).form_valid(form)
 
 class TaskDelete(LoginRequiredMixin,DeleteView):
 	model=Task
