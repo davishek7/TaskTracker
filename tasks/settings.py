65ec471fb0458fa182eca5fb764a28c46efef0f1
Avishek Das <davishek7@gmail.com>
2021-03-14 19:16:41 +0530

    Some fixes

--- tasks/settings.py
+++ tasks/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TaskTracker.urls'
+ROOT_URLCONF = 'tasks.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TaskTracker.wsgi.application'
+WSGI_APPLICATION = 'tasks.wsgi.application'
 
 
 # Database
